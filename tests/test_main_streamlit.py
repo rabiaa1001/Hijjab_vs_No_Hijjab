c9a2088e8177d14d659b5ac18323f6360c5eb657
rabiaa1001 <49293572+rabiaa1001@users.noreply.github.com>
2022-02-27 21:35:02 -0500

    Update test_main_streamlit.py

--- tests/test_main_streamlit.py
+++ tests/test_main_streamlit.py
@@ -8,49 +8,45 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from main_streamlit import load_image_and_resize
 import pytest
 
-@pytest.mark.parametrize('width,height',[(150,150),(224,224),(256,256)])
-def test_load_image_and_resize(width,height):
+
+@pytest.mark.parametrize('width,height', [(150, 150), (224, 224), (256, 256)])
+def test_load_image_and_resize(width, height):
     # Ensure different sizes will be resized as expected
     img = load_image_and_resize(
-        './Test_images/hijjabs0.jpg', width, height)
-    
+        './Test_images/hijjabs0.jpg',
+        width,
+        height)
     # Ensure Image is a PIL.Image.Image subclass
     assert issubclass(type(img), PIL.Image.Image)
-    
     # Assert size of image
     assert img.size == (width, height)
 
-@pytest.mark.parametrize('picture,image_class',[('none4.jpg','No Hijjab'),('hijjab1.jpg','Hijjab')])
-def test_single_image(picture,image_class):
+
+@pytest.mark.parametrize('picture,image_class',
+                         [('none4.jpg', 'No Hijjab'), ('hijjab1.jpg', 'Hijjab')])
+def test_single_image(picture, image_class):
     # Create a class list
     CLS_LIST = ['Hijjab', 'No Hijjab']
-    
     # Retrieve the model
-    MODEL = load_model('hijjab_model_resnet50_v2.h5', compile=False)
-    
+    MODEL = load_model(
+        '../hijjab_model_resnet50_v2.h5',
+        compile=False)
     # Open each image to test
-    img = Image.open(f'./Test_images/{picture}')
-    
+    img = Image.open(
+        f'../Test_images/{picture}')
     # Resize the image for the model
     img = img.resize((224, 224))
-    
     # Convert the image to an array
     x = image.img_to_array(img)
-    
     # Reshape the image
     x = x[:, :, :3]
-    
     # Assert the correct dimensions
     assert x.shape == (224, 224, 3)
-    
     # Preprocess the input
     x = preprocess_input(x)
-    
     # Expand the dimensions
     x = np.expand_dims(x, axis=0)
-    
     # Make a prediction for the image
     pred = MODEL.predict(x)[0]
     pred_class = CLS_LIST[np.argmax(pred)]
-    
     assert pred_class == image_class
